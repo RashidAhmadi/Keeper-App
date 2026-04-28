9862b24fb638770d57242dcd66b2d77d48d4113f
Rashid <rashid.angury20@gmail.com>
2026-04-28 10:43:37 +0200

    Note Components added

---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note(){
+    return (
+    <div className="note">
+        <h1>Thi is a title </h1>
+        <p>this is a paragraph</p>
+
+    </div>
+    );
+}
+
+export default Note;
