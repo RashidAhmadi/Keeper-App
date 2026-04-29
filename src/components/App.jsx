637a4a2471f0369d7a98d6aa3334340844754fa6
Rashid <rashid.angury20@gmail.com>
2026-04-29 13:21:03 +0200

    Navigation bar added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
+import Navbar from "./Navbar";
 function App(){
     return <div>
         <Header/>
+        <Navbar/>
          <Note/>
         <Footer/>
        
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navbar(){
+    return (
+    <nav className="navbar">
+
+      <ul>
+        <li>Home</li>
+        <li>Notes</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+}
+
+
+export default Navbar;
