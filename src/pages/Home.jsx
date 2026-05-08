4b9790ae41c47f640aecf2ef5ab3a29fcce87fd2
Rashid <rashid.angury20@gmail.com>
2026-05-08 11:28:54 +0200

    Arrow function has been used to pass the note data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,14 +12,12 @@ function Home() {
   return (
     <div style={{ padding: "30px" }}>
       <h1>Welcome to Keeper</h1>
-      {notes.map(note=>{
-    return (
-        <Note 
+      {notes.map(note=> <Note 
         key={note.key}
         title={note.title}
         content={note.content}
-    />);
-})}
+    />
+)}
     </div>
   );
 }
